4f244fb25aaa9204b7838265015e84f47395ecbb
Gogonishimwe <floribertgloria@gmail.com>
2024-02-12 09:46:48 +0200

    create hooks file

---
+++ src/test/java/de/framework/hooks/Hooks.java
@@ -0,0 +1,61 @@
+package de.framework.hooks;
+
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+
+import de.framework.utils.DriverManager;
+import de.framework.utils.GlobalParams;
+import de.framework.utils.ServerManager;
+import de.framework.utils.VideoManager;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class Hooks {
+  @Before
+    public void initialize() throws Exception {
+ 
+        // Initialize GlobalParams (capabilities that are likely not to change)
+        GlobalParams params = new GlobalParams();
+        params.initializeGlobalParams();
+ 
+        // Start the server
+        new ServerManager().startServer();
+ 
+        // Initialize the driver
+        new DriverManager().initializeDriver();
+ 
+        // Start recording
+        new VideoManager().startRecording();
+    }
+ 
+    @After
+    public void quit(Scenario scenario) throws IOException {
+ 
+        // Take screenshot if the scenario fails
+        if (scenario.isFailed()) {
+            byte[] screenshot = new DriverManager().getDriver().getScreenshotAs(OutputType.BYTES);
+ 
+            // Attach the screenshot to the scenario report
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+ 
+        // Stop recording
+        new VideoManager().stopRecording(scenario.getName());
+ 
+        // Stop the driver
+        DriverManager driverManager = new DriverManager();
+        if (driverManager.getDriver() != null) {
+            driverManager.getDriver().quit();
+            driverManager.setDriver(null);
+        }
+ 
+        // Stop the server
+        ServerManager serverManager = new ServerManager();
+        if (serverManager.getServer() != null) {
+            serverManager.getServer().stop();
+        }
+    }
+  
+}
